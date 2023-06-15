2710a5b5b61fc34c68fa118a47aa6790111c3233
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-06-15 12:03:38 +0000

    init: initialized ui5 freestyle app

---
+++ ui5-odatav2-demo/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ui5odatav2demo.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ ui5-odatav2-demo/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ui5-odatav2-demo/test/unit/controller/ProductsList.controller"
+], function () {
+	"use strict";
+});
