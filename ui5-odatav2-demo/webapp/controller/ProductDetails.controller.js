9a3a1b3ad29cf389fcb938aa3501a5db12fc6048
chalyshkou <ilya.chalyshkou@leverx.com>
2023-06-19 14:18:19 +0000

    feat: Product Details: * added new app;

---
+++ ui5-odatav2-demo/webapp/controller/ProductDetails.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "./base/BaseController"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("ui5odatav2demo.controller.ProductDetails", {
+        onInit: function () {
+            Controller.prototype.onInit.apply(this);
+        }
+    });
+});
