a2922b77a9f09d6824b4498dc4c194e43942f0f7
chalyshkou <ilya.chalyshkou@leverx.com>
2023-06-20 13:53:12 +0000

    feat: Product Details: * added new route for product details (manifest.json); * rerender page according to the hash value;

--- ui5-odatav2-demo/webapp/controller/ProductDetails.controller.js
+++ ui5-odatav2-demo/webapp/controller/ProductDetails.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    "./base/BaseController"
-], function (Controller) {
+    "./base/BaseController",
+    "sap/f/LayoutType"
+], function (Controller, LayoutType) {
     "use strict";
 
     return Controller.extend("ui5odatav2demo.controller.ProductDetails", {
@@ -9,8 +10,48 @@ sap.ui.define([
             this.getRouter().attachRoutePatternMatched(this._onPatternMatched.bind(this));
         },
 
-        _onPatternMatched() {
-            
-        }
+        async _onPatternMatched(oEvent) {
+            const sRoute = oEvent.getParameters().name;
+            this.productId = oEvent.getParameters()?.arguments?.productId;
+            await this._renderPageContent(sRoute);
+        },
+
+        async _renderPageContent(sRoute) {
+            const fnRenderAction = this._getRenderAction(sRoute);
+            await fnRenderAction();
+        },
+
+        async _showProductInfoFragment() {
+            const oProductInfoText = new sap.m.Text({
+                text: `Product[${this.productId}] info text!!!`
+            });
+
+            oProductInfoText.placeAt(this._getPage(), "only");
+            this.getModel("fcl").setProperty("/layout", LayoutType.TwoColumnsMidExpanded);
+        },
+
+        async _showNoProductFragment() {
+            if (!this.noProductFragment) {
+                this.noProductFragment = await this.loadFragment({ name: "ui5odatav2demo.fragment.ProductDetails.NoProduct" });
+            }
+
+            this.noProductFragment.placeAt(this._getPage(), "only");
+            this.getModel("fcl").setProperty("/layout", LayoutType.TwoColumnsBeginExpanded);
+        },
+
+        _getPage() {
+            return this.getControl("productDetailsPage");
+        },
+
+        _getRenderAction(sRoute) {
+            switch (sRoute) {
+                case "RouteProductsList":
+                    return this._showNoProductFragment.bind(this);
+                case "SelectedProduct":
+                    return this._showProductInfoFragment.bind(this);
+                default:
+                    return this._showNoProductFragment.bind(this);
+            }
+        },
     });
 });
--- ui5-odatav2-demo/webapp/controller/ProductsList.controller.js
+++ ui5-odatav2-demo/webapp/controller/ProductsList.controller.js
@@ -6,6 +6,12 @@ sap.ui.define([
     return Controller.extend("ui5odatav2demo.controller.ProductsList", {
         onInit: function () {
             Controller.prototype.onInit.apply(this);
+        },
+
+        onProductPress(oEvent) {
+            const oRow = oEvent.getSource();
+            const productId = oRow.getBindingContext().getObject().ID;
+            this.navTo("SelectedProduct", { productId });
         }
     });
 });
